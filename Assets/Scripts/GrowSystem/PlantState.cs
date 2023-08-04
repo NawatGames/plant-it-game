677594f80bb4ac24964a993c2aed8143954a98b9
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-08-04 17:16:43 -0300

    Criação de State Machine

---
+++ Assets/Scripts/GrowSystem/PlantState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlantState : MonoBehaviour
+{
+    public string stateName;
+    public UnityEvent stateEnteredEvent;
+    public UnityEvent stateLeavedEvent;
+
+    public void EnterState()
+    {
+        stateEnteredEvent.Invoke();
+    }
+    
+    
+    public void LeaveState()
+    {
+        stateLeavedEvent.Invoke();
+    }
+}
+
+//State Machine, unica responsabilidade da classe é ter um unico estado atual
---
+++ Assets/Scripts/GrowSystem/PlantStateMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlantStateMachine : MonoBehaviour
+{
+    public UnityEvent<PlantState> StateChangedEvent;
+    [SerializeField] private PlantState secondState;
+    [SerializeField] private PlantState initialState;
+    [SerializeField] private PlantState currentState;
+
+    public void SetState(PlantState newState)
+    {
+        if (currentState != null)
+        {
+            currentState.LeaveState();
+        }
+        currentState = newState;
+        StateChangedEvent.Invoke(newState);
+        if (currentState != null)
+        {
+            currentState.EnterState();
+        }
+    }
+
+    public PlantState GetState()
+    {
+        return currentState;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetState(initialState);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            SetState(secondState);
+        }
+    }
+}
