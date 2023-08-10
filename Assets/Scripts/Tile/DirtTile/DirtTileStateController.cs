094174c4c4dbbfa56582652fb222e1b791b4305c
ZettDev <131803879+ZettDev@users.noreply.github.com>
2023-08-09 22:03:18 -0300

    DirtTileStateMachine

---
+++ Assets/Scripts/Tile/DirtTile/DirtTileStateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace DirtTile
+{
+    public class DirtTileStateController : MonoBehaviour
+    {
+        [SerializeField]private TileNutrient waterTileNutrient;
+        [SerializeField]private TileDirtStateMachine dirtStateMachine;
+
+        private void OnEnable()
+        {
+            waterTileNutrient.AmmountChangedEvent.AddListener(OnWaterTileNutrientAmmountChanged);
+        }
+
+        private void OnDisable()
+        {
+            waterTileNutrient.AmmountChangedEvent.RemoveListener(OnWaterTileNutrientAmmountChanged);
+        }
+
+        private void OnWaterTileNutrientAmmountChanged(float arg0)
+        {
+            if (transform.childCount == 0)
+                return;
+            var index = (int)Mathf.Round((transform.childCount - 1) * arg0);
+            dirtStateMachine.SetState(dirtStateMachine.transform.GetChild(index).GetComponent<TileDirtState>());
+        }
+    }
+}
---
+++ Assets/Scripts/Tile/DirtTile/TileDirtState.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace DirtTile
+{
+    public class TileDirtState : MonoBehaviour
+    {
+        public UnityEvent enterEvent; 
+        public UnityEvent leaveEvent;
+        public virtual void Enter()
+        {
+            enterEvent.Invoke();
+        }
+
+        public virtual void Leave()
+        {
+            leaveEvent.Invoke();
+        }
+    }
+}
