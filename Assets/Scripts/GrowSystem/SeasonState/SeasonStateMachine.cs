4047287c1117dac6ce2ea97900ea292cf407bad2
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-08-22 22:00:38 -0300

    State Machine das estações do Ano

---
+++ Assets/Scripts/GrowSystem/SeasonState/SeasonStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SeasonStateMachine : MonoBehaviour
+{
+    public UnityEvent<SeasonState> StateChangedEvent;
+    [SerializeField] private SeasonState currentState;
+
+    public void SetState(SeasonState newState)
+    {
+        if (currentState != null)
+        {
+            currentState.LeaveState();
+        }
+        currentState = newState;
+        StateChangedEvent.Invoke(newState);
+        if (currentState != null)
+        {
+            currentState.EnterState();
+        }
+    }
+
+    public SeasonState GetState()
+    {
+        return currentState;
+    }
+}
