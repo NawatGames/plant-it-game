a67213b0a5034f371460f49077150c6451910b6c
Tutzjwlee <138728172+Tutzjwlee@users.noreply.github.com>
2023-08-27 20:45:38 -0300

    Assingning plant prefabs to unity events PlantLifeStateMachine

--- Assets/Scripts/GrowSystem/AssigningPlantStates/PlantBaseState.cs
+++
@@ -1,8 +0,0 @@
-namespace GrowSystem.AssigningPlantStates
-{
-    public abstract class PlantBaseState 
-    {
-        public abstract void EnterState(PlantStateManager plant);
-        public abstract void UpdateState(PlantStateManager plant);
-    }
-}
