8fbf4c4aa1f9bbb958a5962493579613aaa38b33
Lucas <lucasmgmcalcada@gmail.com>
2023-08-29 22:43:35 -0300

    Screen state machine

---
+++ Assets/Scripts/ScreenStateMachine/ScreenStateMachines.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class ScreenStateMachines : MonoBehaviour
+{
+    public UnityEvent<ScreenState> StateChangedEvent;
+    private ScreenState _currentState;
+    [SerializeField] private ScreenState nextState;
+    [SerializeField] private ScreenState initialState;
+
+    public void Start()
+    {
+        if (initialState != null)
+        {
+            SetState(initialState);
+        }
+    }
+    
+    public void SetState(ScreenState newState)
+    {
+        if (_currentState != null)
+        {
+            _currentState.LeaveState();
+        }
+        _currentState = newState;
+        StateChangedEvent.Invoke(newState);
+        if (_currentState != null)
+        {
+            _currentState.EnterState();
+        }
+    }
+
+    public ScreenState GetState()
+    {
+        return _currentState;
+    }
+
+    [ContextMenu("refresh")]
+    public void Refresh()
+    {
+        SetState(nextState);
+    }
+}
