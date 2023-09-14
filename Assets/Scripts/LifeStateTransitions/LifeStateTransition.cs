cb0d23578fababee1ef7791262a6772a29d5334e
Lucas <lucasmgmcalcada@gmail.com>
2023-09-13 22:32:30 -0300

    LifeStateTransition base class

---
+++ Assets/Scripts/LifeStateTransitions/LifeStateTransition.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeStateTransition : MonoBehaviour
+{
+    [SerializeField] protected PlantLifeStateMachine lifeStateMachine;
+    protected PlantLifeState growing;
+    protected PlantLifeState hibernating;
+    protected PlantLifeState decomposed;
+    protected PlantLifeState dying;
+    protected PlantLifeState ripe;
+    protected PlantLifeState dead;
+
+    private void Awake()
+    {
+        growing = lifeStateMachine.gameObject.transform.Find("Growing").GetComponent<PlantLifeState>();
+        hibernating = lifeStateMachine.gameObject.transform.Find("Hibernating").GetComponent<PlantLifeState>();
+        decomposed = lifeStateMachine.gameObject.transform.Find("Decomposed").GetComponent<PlantLifeState>();
+        dying = lifeStateMachine.gameObject.transform.Find("Dying").GetComponent<PlantLifeState>();
+        ripe = lifeStateMachine.gameObject.transform.Find("Ripe").GetComponent<PlantLifeState>();
+        dead = lifeStateMachine.gameObject.transform.Find("Dead").GetComponent<PlantLifeState>();
+    }
+}
