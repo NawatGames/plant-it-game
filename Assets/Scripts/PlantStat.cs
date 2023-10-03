39fad540f9ae8772acb615c7483d586199184ab2
LucasCalcada <lucasmgmcalcada@gmail.com>
2023-10-02 22:04:32 -0300

    Treshold functions on plant stat

--- Assets/Scripts/PlantStat.cs
+++ Assets/Scripts/PlantStat.cs
@@ -6,6 +6,7 @@ public class PlantStat : MonoBehaviour
     
     [SerializeField] [Range(0f, 1f)] private float currentAmount = 0f;
     public UnityEvent<float> amountChangedEvent;
+    [SerializeField] private float treshold = 0.5f;
     
     public void SetAmount(float amount)
     {
@@ -16,6 +17,16 @@ public class PlantStat : MonoBehaviour
     public float GetAmount()
     {
         return currentAmount;
+    }
+
+    public bool AboveTreshold()
+    {
+        return currentAmount >= treshold;
+    }
+    
+    public bool BelowTreshold()
+    {
+        return currentAmount < treshold;
     }
     
     [ContextMenu("Refresh")]
