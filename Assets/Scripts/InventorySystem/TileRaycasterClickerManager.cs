c487dbb6180024985b8158354f90fd79300f0804
Tutzjwlee <138728172+Tutzjwlee@users.noreply.github.com>
2023-10-06 23:26:34 -0300

    To conseguindo

--- Assets/Scripts/InventorySystem/TileRaycasterClickerManager.cs
+++ Assets/Scripts/InventorySystem/TileRaycasterClickerManager.cs
@@ -2,6 +2,7 @@
 using InputSystem;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.PlayerLoop;
 
 namespace InventorySystem
 {
@@ -16,9 +17,12 @@ namespace InventorySystem
         [SerializeField] private LayerMask layerMask;
         public UnityEvent HandlerSuccessEvent;
         public UnityEvent HandlerFailEvent;
+        private Camera cameraMain;
+        private Vector2 _mousePosition;
         private void Awake()
         {
             _inputManager = GameObject.FindFirstObjectByType<InputManager>();
+            cameraMain = Camera.main;
         }
 
         private void OnEnable()
@@ -33,6 +37,7 @@ namespace InventorySystem
         private void UpdateMousePosition(Vector2 arg0)
         {
             mousePosition = arg0;
+            _mousePosition = arg0;
         }
         /*
          * Escuta quando o mouse esquerdo é solto
@@ -41,35 +46,47 @@ namespace InventorySystem
          * Chama o evento de click do tile
          */
         private void OnLeftButtonChanged(bool arg0)
-        {
+        {   Debug.Log("TileRaycasterClickerManager: OnLeftButtonChanged");
             if (arg0) return;
             var isMouseAboveUI = UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
             if (isMouseAboveUI) return;
-            
+            Debug.Log("TileRaycasterClickerManager: mouse is not above UI " );
             // Testar se o contact filter ta funcionando direito
             // Se não, boa sorte
             ContactFilter2D contactFilter2D = new ContactFilter2D();
-            contactFilter2D.layerMask = layerMask;
-            //contactFilter2D.SetLayerMask(layerMask);
-                    
+            contactFilter2D.SetLayerMask(layerMask);
             List<RaycastHit2D> results = new List<RaycastHit2D>();
-            Physics2D.Raycast(mousePosition, Vector2.zero, contactFilter2D, results);
+            // Physics2D.Raycast(mousePosition, Vector2.zero, contactFilter2D, results);
+            Physics2D.Raycast(cameraMain.ScreenToWorldPoint(_mousePosition),
+                Vector2.zero, contactFilter2D, results);
+            Debug.Log("Mouse Position: " + mousePosition);
+            Debug.Log("Number of hits: " + results.Count);
+            
             foreach (RaycastHit2D i in results)
-            {
+            {   Debug.Log("TileRaycasterClickerManager: Raycast loop ");
                 GameObject target = i.collider.gameObject;
-                TileHandler handler = target.GetComponent<TileHandler>();
+                TileHandler handler = target.GetComponentInChildren<TileHandler>();
                 if (handler != null)
+                {
+                    Debug.Log("TileRaycasterClickerManager: TileHandler found");
+                    if (handler.tileClicker != null)
+                    {
+                        handler.tileClicker.Click();
+                    }
+                    else
+                    {
+                        Debug.LogWarning("TileClicker not found on TileHandler");
+                    }
+
+                    HandlerSuccessEvent.Invoke();
+                    Debug.Log("TileRaycasterClickerManager: TileHandler found");
+                }
+                else
                 {
                     HandlerFailEvent.Invoke();
                     Debug.Log("TileRaycasterClickerManager: TileHandler not found");
                 }
-                else
-                {
-                    handler.tileClicker.Click();
-                    HandlerSuccessEvent.Invoke();
-                    Debug.Log("TileRaycasterClickerManager: TileHandler found");
-                }
             }
-        }
+        } 
     }
 }
