9b90776b510111f29c3cf678a6d902845435532c
ZettDev <131803879+ZettDev@users.noreply.github.com>
2023-12-14 20:54:21 -0300

    Missing interpreters

---
+++ Assets/Scripts/Database/Interpreters/CountedDropsInterpreter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Database.Contents;
+using UnityEngine;
+
+namespace Database.Interpreters
+{
+    public class CountedDropsInterpreter : MonoBehaviour
+    {
+        public TextAsset countedDropsDatabase;
+        [SerializeField] private List<CountedDrops> items = new List<CountedDrops>();
+        private void Awake()
+        {
+            var text = countedDropsDatabase.text;
+            
+            // Splits the CSV into lines
+            var lines = text.Split('\n').ToList();
+            
+            // Removes invalid lines
+            lines.RemoveAt(0);
+            lines.RemoveAt(lines.Count - 1);
+            
+            // Creates a fertilizer item for each line
+            foreach (var line in lines)
+            {
+                Debug.Log(line);
+                CountedDrops item = new CountedDrops(line);
+                items.Add(item);
+            }
+        }
+        
+        public CountedDrops GetItem(int id)
+        {
+            return items.FirstOrDefault(countedDrops => countedDrops.id == id);
+        }
+    }
+}
---
+++ Assets/Scripts/Database/Interpreters/GuaranteedDropsInterpreter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Database.Contents;
+using UnityEngine;
+
+namespace Database.Interpreters
+{
+    public class GuaranteedDropsInterpreter : MonoBehaviour
+    {
+        public TextAsset guaranteedDropsDatabase;
+        [SerializeField] private List<GuaranteedDrops> items = new List<GuaranteedDrops>();
+        private void Awake()
+        {
+            var text = guaranteedDropsDatabase.text;
+            
+            // Splits the CSV into lines
+            var lines = text.Split('\n').ToList();
+            
+            // Removes invalid lines
+            lines.RemoveAt(0);
+            lines.RemoveAt(lines.Count - 1);
+            
+            // Creates a fertilizer item for each line
+            foreach (var line in lines)
+            {
+                Debug.Log(line);
+                GuaranteedDrops item = new GuaranteedDrops(line);
+                items.Add(item);
+            }
+        }
+        
+        public GuaranteedDrops GetItem(int id)
+        {
+            return items.FirstOrDefault(guaranteedDrops => guaranteedDrops.id == id);
+        }
+    }
+}
