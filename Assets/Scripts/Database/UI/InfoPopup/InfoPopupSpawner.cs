8a92f474f78a021d66085393e467a885353084c7
LucasCalcada <lucasmgmcalcada@gmail.com>
2023-12-14 22:28:39 -0300

    Database UI

---
+++ Assets/Scripts/Database/UI/InfoPopup/InfoPopupSpawner.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Database.UI.InfoPopup
+{
+    public class InfoPopupSpawner : MonoBehaviour
+    {
+        
+    }
+}
--- Assets/Scripts/UI/Inventory/ItemUIInjector.cs
+++ Assets/Scripts/UI/Inventory/ItemUIInjector.cs
@@ -1,4 +1,6 @@
+using Database;
 using Database.Contents;
+using Database.ItemCategories;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,13 +9,26 @@ namespace UI.Inventory
 {
     public class ItemUIInjector : MonoBehaviour
     {
-        [SerializeField] private Image itemImage;
-        [SerializeField] private Image itemFrame;
-        [SerializeField] private TMP_Text itemName;
+        [SerializeField] private GameObject injectorsHolder;
+        [SerializeField] private RawImage itemImage;
+        [SerializeField] private RawImage itemFrame;
+        [SerializeField] private TMP_Text label;
         [SerializeField] private TMP_Text itemAmount;
-        public void Inject(Item item, int amount)
+        public void Inject(string itemName, int amount)
         {
-            
+            Item item = DatabaseSingleton.Instance.itemIntrepeter.GetItem(itemName);
+            if(item == null)
+            {
+                Debug.LogError("Item not found");
+                return;
+            }
+            itemImage.texture = item.itemTexture;
+            //itemFrame.texture = item.itemFrame;
+            label.text = item.itemName;
+            itemAmount.text = amount.ToString();
+            string categoryName = item.category.ToString();
+            GameObject injector = injectorsHolder.transform.Find(categoryName).gameObject;
+            injector.GetComponent<CategoryInjector>().Inject(itemName);
         }
     }
 }
