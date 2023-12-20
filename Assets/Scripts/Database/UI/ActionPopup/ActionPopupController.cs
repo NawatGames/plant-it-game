2f8081c4191fa22cdcb0f606eeb88f00ce0beb71
Tutzjwlee <138728172+Tutzjwlee@users.noreply.github.com>
2023-12-19 23:59:25 -0300

    nothing much

--- Assets/Scripts/Database/UI/ActionPopup/ActionPopupController.cs
+++ Assets/Scripts/Database/UI/ActionPopup/ActionPopupController.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 
 namespace Database.UI.ActionPopup
 {
-    public class ActionPopupController : MonoBehaviour
+    public class ActionPopupController : MonoBehaviour, IPointerClickHandler
     {
         public UnityEvent actionOpenPopupEvent;
         public UnityEvent closePopupEvent;
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            actionOpenPopupEvent.Invoke();
+            Debug.Log("OpenPopup");
+            
+        }
     }
 }
